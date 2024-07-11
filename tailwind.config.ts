6b220eeb46f251b2808ef01b90688b28b3901862
Michal Srb <srb@convex.dev>
2024-07-11 15:10:52 +0200

    Fix Tailwind config for dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class"],
+  darkMode: ["selector"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: ["dark"],
   theme: {
     container: {
       center: true,
